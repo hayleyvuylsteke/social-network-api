dd9d32cccc034866427f3a96ca4afed88f9d04a1
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-04-07 12:47:15 -0400

    pushing updated code

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 const { addThought, editThought, removeThought } = require('../../controllers/thought-controllers');
 
+router.route('/:pizzaId').post(addThought);
+router.route('/:pizzaId').put(editThought);
+
+router.route('/:pizzaId/:commentId').delete(removeThought);
+
 module.exports = router;
